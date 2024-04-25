5ee3d9e55a4affabb87b75d7811a12d572ea4b95
zhuowenzhao <zwenzhao11@gmail.com>
2024-04-25 15:30:31 -0700

    initialize progress from 0%

--- components/progress.py
+++ components/progress.py
@@ -14,7 +14,7 @@ def layout():
                             style={"font-weight": "bold"}
                         ),
                         dbc.Label(id='total-labeled', children='Labeled 100 out of 1000 tomograms', style={'margin-top': '5px', 'margin-left': '15px', 'margin-bottom': '-5px'}),
-                        dbc.CardBody([dbc.Progress(id='progress-bar', label="10%", value=10)])
+                        dbc.CardBody([dbc.Progress(id='progress-bar', label="0%", value=0)])
                     ],
                     style={"height": '20%'})
     return header
