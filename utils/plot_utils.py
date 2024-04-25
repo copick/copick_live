625bf5b795ae1bb1ad16bcd22344fd6946f44463
zhuowenzhao <zwenzhao11@gmail.com>
2024-04-25 15:30:02 -0700

    clear prints

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -4,8 +4,6 @@ import plotly.express as px
 with open('assets/copick_config_default.json') as f:
     config = json.load(f)
 
-print(config["pickable_objects"])
-
 def barplot(x, y, xlabel=None, ylabel=None, colors: bool | list=None):
     if colors:
         fig = px.bar(x=x, 
