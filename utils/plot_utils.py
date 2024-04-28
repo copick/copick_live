5d1923ae2f4ae6e8488567535ca41b36271b0908
zhuowenzhao <zwenzhao11@gmail.com>
2024-04-27 23:43:16 -0700

    rm unnecessary blobs

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,9 +1,5 @@
 import json
 import plotly.express as px
-from utils.data_utils_threading import COPICK_CONFIG_PATH
-
-with open(COPICK_CONFIG_PATH) as f:
-    config = json.load(f)
 
 
 def barplot(x, y, xlabel=None, ylabel=None, colors: bool | list=None):
